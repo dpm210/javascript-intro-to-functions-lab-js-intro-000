88365487cd64ae2b39220c7454883bd7f219e242
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-05 11:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logShout(string){
   console.log(uppercase);
 }
 function logWhisper(string){
-  lowecase = string.toLowerCase()
+  lowercase = string.toLowerCase()
   string = lowercase;
   console.log(lowercase);
 }
